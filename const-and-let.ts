fe0c8844fcf73cdceca018d897db1ac9f88af162
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2026-01-19 00:11:16 -0300

    const and let

---
+++ const-and-let.ts
@@ -0,0 +1,9 @@
+const firstName = "Rick";
+// firstName = "Morty";
+// TypeError: Assignment to constant variable.
+//Cannot assign to 'firstName' because it is a constant.
+
+let age = 17;
+age = 18;
+console.log(age);
+// output -> 18
