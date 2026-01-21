7fb79d4b4dbe6ff77953fbc869e6410f510f0f54
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2026-01-21 00:15:36 -0300

    default parameters

--- template-literals.ts
+++ template-literals.ts
@@ -10,4 +10,10 @@ const returnPersonES6 = (name: string, age: number) => {
   return `Hi ${name}, your age is ${age} year old!`;
 };
 console.log(returnPersonES6("Liz", 32));
+// output: Hi Liz, your age is 32 year old!
 
+const returnPersonDefaultAgeES6 = (name: string, age = 22) => {
+  return `Hi ${name}, your age is ${age} year old?`;
+};
+console.log(returnPersonDefaultAgeES6("Carl"));
+// output: Hi Carl, your age is 22 year old?
