c39ef0cd2b02ddc2ba33c52d6a05f6e0baf4c327
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2026-01-29 22:02:39 -0300

    looping a set

--- sets.ts
+++ sets.ts
@@ -11,3 +11,13 @@ letters.add("c");
 
 console.log(letters);
 // output: Set(3) { 'a', 'b', 'c' }
+
+
+// List all Elements
+let text = "";
+for (const x of letters) {
+  text += x;
+}
+
+console.log(text);
+// output: abc
