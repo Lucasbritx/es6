bf3ab7d645252ecbcd4b59239db8f70bb9629cc4
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2026-01-29 22:01:28 -0300

    Duplicates in a set

--- sets.ts
+++ sets.ts
@@ -6,5 +6,8 @@ letters.add("a");
 letters.add("b");
 letters.add("c");
 
+// Set don't allow multiple values, the output will be the same a, b, c
+letters.add("c");
+
 console.log(letters);
 // output: Set(3) { 'a', 'b', 'c' }
